acd4fccd96238e8d9a3c2552973362269f9a0564
dk872 <143122117+dk872@users.noreply.github.com>
2024-03-07 18:45:40 +0200

    The elements, the scoreboard, and the font size are set

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const app = function () {
   
   function init() {
     console.log('init ready');
+    game.main = document.querySelector('#game');
+    console.log(game);
+    game.scoreboard = document.createElement('div');
+    game.scoreboard.textContent = "Dealer 0 vs Player 0";
+    game.scoreboard.style.fontSize = "2em";
+    game.main.append(game.scoreboard);
   }
   return {
     init: init
