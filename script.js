ad96832fd45225ad9aec5f1101a53f6c1fcec0f1
dk872 <143122117+dk872@users.noreply.github.com>
2024-03-22 16:40:11 +0200

    Updated 'ranks'

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const app = function () {
   const game = {};
   const suits = ["spades", "hearts", "clubs", "diams"];
-  const ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  //const ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const ranks = ["A", "A", "A", "A", "A", "K"];
 
   function init() {
     console.log('init ready');
